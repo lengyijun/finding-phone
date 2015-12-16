30d337a0e6edda137e5d997bb832e0517f86d4a3
sjtu-lyj <sjtu5140809011@gmail.com>
2015-12-16 22:29:29 +0800

    make some change in ui

--- gen/com/example/fragmentdemo/R.java
+++ gen/com/example/fragmentdemo/R.java
@@ -33,17 +33,17 @@ public final class R {
         public static final int tab_bg=0x7f020009;
     }
     public static final class id {
-        public static final int action_settings=0x7f0a0015;
-        public static final int button1=0x7f0a000f;
-        public static final int button2=0x7f0a0013;
+        public static final int action_settings=0x7f0a0018;
+        public static final int button1=0x7f0a0013;
+        public static final int button2=0x7f0a0016;
         public static final int contacts_image=0x7f0a0005;
         public static final int contacts_layout=0x7f0a0004;
         public static final int contacts_text=0x7f0a0006;
         public static final int content=0x7f0a0000;
-        public static final int editText1=0x7f0a0012;
-        public static final int editText2=0x7f0a0011;
-        public static final int et_test=0x7f0a0014;
-        public static final int linearLayout1=0x7f0a000e;
+        public static final int editText1=0x7f0a0015;
+        public static final int editText2=0x7f0a0014;
+        public static final int et_test=0x7f0a0017;
+        public static final int linearLayout1=0x7f0a000d;
         public static final int message_image=0x7f0a0002;
         public static final int message_layout=0x7f0a0001;
         public static final int message_text=0x7f0a0003;
@@ -53,8 +53,11 @@ public final class R {
         public static final int setting_image=0x7f0a000b;
         public static final int setting_layout=0x7f0a000a;
         public static final int setting_text=0x7f0a000c;
-        public static final int textView1=0x7f0a000d;
-        public static final int textView2=0x7f0a0010;
+        public static final int textView1=0x7f0a000e;
+        public static final int textView2=0x7f0a000f;
+        public static final int textView3=0x7f0a0010;
+        public static final int textView4=0x7f0a0011;
+        public static final int textView5=0x7f0a0012;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
